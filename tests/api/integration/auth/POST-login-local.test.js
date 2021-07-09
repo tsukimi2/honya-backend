d2454439c4920c307427f91f53f351ae5f334ee9
tsukimi2 <shigotoganbatte4@gmail.com>
2021-07-09 16:32:43 +0800

    testing

--- tests/api/integration/auth/POST-login-local.test.js
+++ tests/api/integration/auth/POST-login-local.test.js
@@ -21,17 +21,15 @@ describe(API_PREFIX + '/login', () => {
       console.log(err)
     }
 
-    await request(app)
+    request(app)
       .post(API_PREFIX + '/register')
       .send(`username=${user.username}`)
       .send(`password=${user.password}`)
       .send(`email=${user.email}`)
       .set('Accept', 'application/json')
-      /*
       .end(function(err, res) {
         if (err) throw err;
       })
-      */
   })
 
   beforeEach(async () => {
