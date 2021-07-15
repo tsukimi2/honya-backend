436c33818ae3a97412d09574c06ba296cf5ba021
tsukimi2 <shigotoganbatte4@gmail.com>
2021-07-15 18:30:56 +0800

    Added errHandler unit tests and changed errHandler into factory function
    - Added errHandler unit tests
    - Changed errHandler from export function into factory function to allow dependency injection into errHandler

--- src/di-container.js
+++ src/di-container.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose'
 import config from './libs/config/index.js'
 import logger from './libs/logger/index.js'
 import Database from './libs/database/index.js'
+import ErrHandler from './libs/errHandler.js'
 import AuthController from './auth/auth.controller.js'
 import UserController from './user/user.controller.js'
 import UserService from './user/user.service.js'
 import UserRepos from './user/user.repos.js'
 import User from './user/user.model.js'
 
+export const errHandler = ErrHandler({ logger })
 export const database = Database({ mongoose })
 export const userRepos = new UserRepos(User)
 export const userService = UserService({ userRepos })
--- src/libs/errHandler.js
+++ src/libs/errHandler.js
@@ -1,10 +1,10 @@
 import UserFacingError from '../errors/UserFacingError.js';
 import DatabaseError from '../errors/DatabaseError.js';
-import logger from '../libs/logger/index.js'
+// import logger from '../libs/logger/index.js'
 import BaseError from '../errors/BaseError.js';
 
 export const isOperationalError = (err) => (err instanceof BaseError) ? err.isOperational : false
-
+/*
 const errHandler = (err, req, res, next) => {
   logger.error(err)
   logger.error(err.name)
@@ -29,6 +29,41 @@ const errHandler = (err, req, res, next) => {
     err: 'InternalServerErr',
     errmsg: 'Internal server error'
   })
+}
+
+export default errHandler
+*/
+
+const errHandler = ({ logger }) => {
+  const process = (err, req, res, next) => {
+    logger.error(err)
+    logger.error(err.name)
+    logger.error(err.message)
+    logger.error(err.stack)
+    if(err.err) {
+      logger.log(err.err)
+    }
+    if(err instanceof UserFacingError) {
+      return res.status(err.statusCode).json({
+        err: err.name,
+        errmsg: err.message
+      })
+    } else if(err instanceof DatabaseError) {
+      return res.status(err.statusCode).json({
+        err: err.name,
+        errmsg: err.message
+      })
+    }
+  
+    return res.status(500).json({
+      err: 'InternalServerErr',
+      errmsg: 'Internal server error'
+    })
+  }
+
+  return {
+    process
+  }
 }
 
 export default errHandler
