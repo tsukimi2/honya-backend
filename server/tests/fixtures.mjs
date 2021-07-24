9ca3e3cd255f346d72e896cc119eed88fee6e12a
tsukimi2 <shigotoganbatte4@gmail.com>
2021-07-24 06:22:09 -0700

    Added nginx into docker-compose

--- server/tests/fixtures.mjs
+++ server/tests/fixtures.mjs
@@ -3,7 +3,5 @@ import config from '../src/libs/config/index.js'
 
 export async function mochaGlobalSetup() {
   const dburi = `mongodb://${config.get('db:mongo:host')}:${config.get('db:mongo:port')}/${config.get('db:mongo:schema')}`
-console.log('dburi')  
-console.log(dburi)
   database.connect(dburi)
 }
