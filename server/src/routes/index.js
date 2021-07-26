cd48654c0a05824c169f181875da5cfe32e0a435
tsukimi2 <shigotoganbatte4@gmail.com>
2021-07-25 21:07:16 -0700

    testing

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,6 +5,9 @@ import userRoutes from '../user/user.routes.js'
 const API_PREFIX = config.get('app:api_prefix')
 
 export function bindRoutes(app) {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'ok' })
+  })
   app.use(API_PREFIX, authRoutes)
   app.use(API_PREFIX, userRoutes)
 }
