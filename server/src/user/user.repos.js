90cb016f7c84a9c846161447255640cc873e8179
tsukimi2 <shigotoganbatte4@gmail.com>
2021-07-27 22:27:27 -0700

    Added test cases and bug fixes in user unit tests
    - Added unit tests for getUser and createUser
    - Bug fix in getUserById code and associated unit tests

--- server/src/user/user.repos.js
+++ server/src/user/user.repos.js
@@ -1,22 +1,38 @@
 import _ from 'lodash'
 import NotFoundError from '../errors/NotFoundError.js'
+import ApplicationError from '../errors/ApplicationError.js'
 
 export default class UserRepos {
   constructor(model) {
     this.model = model
   }
 
+  setModel(model) {
+    this.model = model
+  }
+
   async getUserById(id, opts={}) {
+    if(!id) {
+      throw new ApplicationError('Invalid user id')
+    }
+
     return opts.lean ? await this.model.findById(id).lean() : await this.model.findById(id).exec()
   }
 
   async getUser(filterParams, opts={}) {
+    if(!filterParams || _.isEmpty(filterParams)) {
+      throw new ApplicationError('Invalid user params')
+    }
+
     return opts.lean ? await this.model.findOne(filterParams).lean() : await this.model.findOne(filterParams).exec()
   }
 
-  async createUser(params) {
+  async createUser(params) { 
+    if(!params || _.isEmpty(params)) {
+      throw new ApplicationError('Invalid user params')
+    }
+
     let user = new this.model
-    
     for (const [key, value] of Object.entries(params)) {
       user[key] = value
     }
