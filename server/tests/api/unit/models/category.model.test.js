78b34ac30eccdad8c5356a5644df2cc0ae873aa0
tsukimi2 <shigotoganbatte4@gmail.com>
2021-07-31 18:27:11 -0700

    Removed unused comments from category.model.test.js

--- server/tests/api/unit/models/category.model.test.js
+++ server/tests/api/unit/models/category.model.test.js
@@ -81,21 +81,3 @@ describe('Category model', () => {
 
   })
 })
-
-
-
-/*
-
-const categorySchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-            maxlength: 32,
-            unique: true
-        }
-    },
-    { timestamps: true }
-);
-*/
