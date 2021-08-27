b459ae4cbae535bd2247792d7353fbc61317e1cf
tsukimi2 <shigotoganbatte4@gmail.com>
2021-08-26 23:26:25 -0700

    Added some e2e category test cases

--- server/tests/e2e/category/category-list.test.js
+++ server/tests/e2e/category/category-list.test.js
@@ -7,17 +7,27 @@ import { connectToMongodb } from '../../libs/mongodb.js'
 import { generateUserParams } from '../../factories/userFactory.js'
 
 
-describe('e2e category', () => {
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  })
+}
+
+describe.skip('e2e category', () => {
   let page = null
   let loginPage = null
   //const loginUrl = 'http://honya.co.jp/signin'
   const loginUrl = `${config.get('uri:test_base_href')}/signin`
+  const createCategoryUrl = `${config.get('uri:test_base_href')}/admin/category`
+
   let userParams = null
   let adminParams = null
 
   let dbclient = null
   let db = null
   let usersCollection = null
+  let categoriesCollection = null
+  let expectedCategories = []
 
   before(async () => {
     dbclient = await connectToMongodb()
@@ -29,9 +39,18 @@ describe('e2e category', () => {
 
     userParams = await generateUserParams({ userProfile: 'validUser1', hasHashedPassword: true  })
     adminParams = await generateUserParams({ userProfile: 'validAdmin1', hasHashedPassword: true  })
-  
-    await usersCollection.insertOne(userParams)
-    await usersCollection.insertOne(adminParams)
+    const users = [userParams, adminParams]
+    // await usersCollection.insertOne(userParams)
+    await usersCollection.insertMany(users)
+
+    categoriesCollection = db.collection('categories')
+   
+    await categoriesCollection.deleteMany({ name: /^cat*/ })
+    expectedCategories = [
+      { name: 'cat1' },
+      { name: 'cat2' }
+    ]
+    // await categoriesCollection.insertMany(expectedCategories)
 
     page = await Page.build('Desktop')
     loginPage = await new LoginPage(page)
@@ -39,34 +58,49 @@ describe('e2e category', () => {
 
   after(async () => {
     await page.close()
-    await usersCollection.deleteMany({ "username": /^user*/ })
-    await usersCollection.deleteMany({ "username": /^admin*/ })
+    //await usersCollection.deleteMany({ "username": /^user*/ })
+    //await usersCollection.deleteMany({ "username": /^admin*/ })
+    //await categoriesCollection.deleteMany({ name: /^cat*/ })
 
     await dbclient.close()
   })
 
   describe('Able to access Category page with admin credentials', () => {
     before(async () => {
-      await page.goto('loginUrl')
-      await loginPage.login(adminParams.username, adminParams.passowrd)
+      await page.goto(loginUrl)   
+      await loginPage.login(adminParams.username, adminParams.password)
+
+      await page.goto(createCategoryUrl)
     })
 
     after(async () => {
-
+      // await page.waitAndClick('button[type="button"][class^="menu_signoutBtn"]')
     })
+
 
     describe('e2e list categories', () => {
       context('should render category list when there are categories in db', () => {
-    
-      })
-    
-      context('should render empty list when no categories in db', () => {
-        
+        it('should render category list when there are categories in db', async () => {
+          //const numItems = await page.getCount('div[class^="CrudListItem_listItem"]')
+          //expect(numItems).to.eq(expectedCategories.length)
+        })
       })
     })
 
-    describe('e2e add category', () => {
 
+    describe('e2e add category', async () => {
+      it('should add new category into category list', async () => {
+        await delay(1000)
+console.log('kon')        
+        await page.goto(createCategoryUrl)
+console.log('kon2')          
+        await delay(1000)
+        await page.waitAndType('#name', 'cat1')
+console.log('kon3')          
+        await page.waitAndClick('button[type="submit"]')
+        const numItems = await page.getCount('div[class^="CrudListItem_listItem"]')
+        expect(numItems).to.have.lengthOf(1)
+      })
     })
 
     describe('e2e edit category', () => {
